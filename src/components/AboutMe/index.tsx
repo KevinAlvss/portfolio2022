4f671d8f8cda6fcb15688c72dca5e355596494ae
KevinAlvss <kevin.bernardo64@gmail.com>
2022-08-18 10:16:45 -0300

    add animations in about me

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import Aos from "aos";
+
 import {
   Container,
   Content,
@@ -11,16 +14,32 @@ import {
 import delevoper from "../../assets/developer.svg";
 
 export function AboutMe() {
+  useEffect(() => {
+    Aos.init({
+      duration: 800,
+      easing: "ease-in-out",
+    });
+  }, []);
+
   return (
     <Container>
       <Content>
         <ImageContainer>
-          <img src={delevoper} alt="dev" />
+          <img
+            src={delevoper}
+            alt="dev"
+            data-aos="fade-down"
+            data-aos-delay="300"
+          />
         </ImageContainer>
         <TextContainer>
-          <Title>Sobre mim</Title>
-          <Location>São Paulo, Brasil</Location>
-          <Text>
+          <Title data-aos="fade-down" data-aos-delay="300">
+            Sobre mim
+          </Title>
+          <Location data-aos="fade-down" data-aos-delay="400">
+            São Paulo, Brasil
+          </Location>
+          <Text data-aos="fade-down" data-aos-delay="500">
             Olá! Muito prazer! Eu sou o Kevin, uma pessoa apaixonada por
             tecnologia, e por programação! Já trabalho com desenvolvimento
             fullstack há 1 ano e 5 meses, usando tecnologias como: ReactJs,
